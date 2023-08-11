95e21414b25b858a104fddeadf4fe16573d430e9
JeongHyeon-Yoon <feel2dia@naver.com>
2023-08-11 10:00:38 +0900

    #10 - 스프링부트 프로젝트 생성
    프로젝트 진행을 위한 기본 프로젝트 생성
    프로젝트 의존성은 아래 항목을 추가
    * actuator
    * jpa
    * rest
    * hal-explorer
    * lombok
    * devtools
    * h2
    * mysql-connector

---
+++ src/main/java/com/yoondev/ordermgrapi/OrderMgrApiApplication.java
@@ -0,0 +1,13 @@
+package com.yoondev.ordermgrapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderMgrApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderMgrApiApplication.class, args);
+	}
+
+}
